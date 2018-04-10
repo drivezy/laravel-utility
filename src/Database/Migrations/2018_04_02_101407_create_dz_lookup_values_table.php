80b61caafb6984461776e75e35d27c58204dc872
Hemant Kumar Sah <hemantanshu@gmail.com>
2018-04-10 11:20:53 +0530

    Fixed the column name

--- src/Database/Migrations/2018_04_02_101407_create_dz_lookup_values_table.php
+++ src/Database/Migrations/2018_04_02_101407_create_dz_lookup_values_table.php
@@ -5,36 +5,33 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateHmPropertiesTable extends Migration {
+class CreateDzLookupValuesTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
     public function up () {
-        Schema::create('dz_properties', function (Blueprint $table) {
+        Schema::create('dz_lookup_values', function (Blueprint $table) {
             $userTable = ( new User() )->getTable();
 
             $table->increments('id');
+            $table->unsignedInteger('lookup_type_id')->nullable();
 
-            $table->string('key');
-            $table->string('value');
-
-            $table->boolean('caching_enabled')->default(true);
-            $table->boolean('is_public')->default(false);
-
+            $table->string('name');
+            $table->text('value');
             $table->string('description')->nullable();
 
             $table->unsignedInteger('created_by')->nullable();
             $table->unsignedInteger('updated_by')->nullable();
+
+            $table->foreign('lookup_type_id')->references('id')->on('dz_lookup_types');
 
             $table->foreign('created_by')->references('id')->on($userTable);
             $table->foreign('updated_by')->references('id')->on($userTable);
 
             $table->timestamps();
             $table->softDeletes();
-
-
         });
     }
 
@@ -44,6 +41,6 @@ class CreateHmPropertiesTable extends Migration {
      * @return void
      */
     public function down () {
-        Schema::dropIfExists('dz_properties');
+        Schema::dropIfExists('dz_lookup_values');
     }
 }
