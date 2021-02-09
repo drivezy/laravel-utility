25ac6c8b7f7b826ed1f0b94ca1124073f222bc53
Hemant Kumar Sah <hemantanshu@gmail.com>
2021-02-09 11:28:17 +0530

    fixed issue with comment detail observer

--- src/Observers/CommentDetailObserver.php
+++ src/Observers/CommentDetailObserver.php
@@ -1,17 +1,12 @@
 <?php
 
-namespace JRApp\Observers\Utility;
+namespace Drivezy\LaravelUtility\Observers;
 
-use JRApp\Libraries\Utility\MaintenanceManagement;
-use JRApp\Models\Accounts\ExpenseVoucher;
-use JRApp\Models\Utility\ServicingHistory;
-use JRApp\Models\Utility\Vendor;
 use Drivezy\LaravelUtility\Observers\BaseObserver;
-use Illuminate\Database\Eloquent\Model as Eloquent;
 
 /**
  * Class CommentDetailObserver
- * @package JRApp\Observers\Utility
+ * @package Drivezy\LaravelUtility\Observers
  */
 class  CommentDetailObserver extends BaseObserver
 {
@@ -21,28 +16,4 @@ class  CommentDetailObserver extends BaseObserver
     protected $rules = [
         'comments' => 'required',
     ];
-
-    /**
-     * @param Eloquent $model
-     */
-    public function created (Eloquent $model)
-    {
-        parent::created($model);
-
-        if ( $model->source == 'ServicingHistory' ) {
-            $vendor = Vendor::where('user_id', $model->created_by)->first();
-            if ( !$vendor ) {
-                $servicing = ServicingHistory::find($model->source_id);
-                if ( $servicing ) {
-                    MaintenanceManagement::updateServicingTicket($servicing, $model->comments);
-                }
-            }
-        } elseif ( $model->source == 'ExpenseVoucher' && ( strpos($model->comments, 'status updated to') == false ) ) {
-            $expenseVoucher = ExpenseVoucher::where('id', $model->source_id)->where('state', 1070)->first();
-            if ( $expenseVoucher ) {
-                $expenseVoucher->state = 276;
-                $expenseVoucher->save();
-            }
-        }
-    }
 }
